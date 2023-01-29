474ae0e6bc305979baf7bcc66ed05d335cf4955f
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2023-01-29 14:27:08 -0800

    built release

--- mindcraft-pdf-to-book/Program.cs
+++ mindcraft-pdf-to-book/Program.cs
@@ -7,42 +7,8 @@ namespace PdfToMineCraftBook
         const int MAX = 99;
         static void Main(string[] strings)
         {
-            Console.WriteLine("Welcome! Let's get started.");
-            TestPDF();
-        }
-
-        public static void TestPDF()
-        {
             UX ux = new UX();
             ux.Init();
-
-            /*PDFConversion doc= new PDFConversion(Test.Path, Test.Remove);
-            doc.LoadPDF();
-            doc.ViewPDFText();
-            var book = doc.GenerateBook();
-
-            if (book != null)
-            {
-                _ = WriteTextFile(book);
-            }
-            else 
-            {
-                Console.WriteLine("We didn't make a book!");
-            }
-
-            Console.WriteLine("Done!");
-*/
         }
-
-        
-
-
     }
-
-/*    internal class Test
-    {
-        public static string Path = @"D:\OneDrive\Documents\C#Projects\MineCraft-pdf-to-book\test-examples\7_ThereWillComeSoftRainsbyRayBradbury.pdf";
-        public static string[] Remove = { "1/25/13Ray Bradbury: There Will Come Soft Rainswww.dennissylvesterhurd.com/blog/softrain.htm" };
-    }*/
 }
-
