1e2d4edd99694518ea5f91fa3b029ad2df696981
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2023-01-29 14:35:04 -0800

    removed test method from ux

--- mindcraft-pdf-to-book/ux.cs
+++ mindcraft-pdf-to-book/ux.cs
@@ -236,61 +236,6 @@ namespace PdfToMineCraftBook
             
         }
 
-        public void Test()
-        {
-
-            AnsiConsole.Write(
-            new FigletText("Scaffold-demo")
-                .Color(Color.Red));
-
-            Console.WriteLine("");
-            var answerReadme = AnsiConsole.Confirm("Generate a [green]README[/] file?");
-            var answerGitIgnore = AnsiConsole.Confirm("Generate a [yellow].gitignore[/] file?");
-
-            Console.WriteLine("");
-            answerReadme = AnsiConsole.Confirm("Generate a [green]README[/] file?");
-            answerGitIgnore = AnsiConsole.Confirm("Generate a [yellow].gitignore[/] file?");
-
-            var framework = AnsiConsole.Prompt(
-              new SelectionPrompt<string>()
-                  .Title("Select [green]test framework[/] to use")
-                  .PageSize(10)
-                  .MoreChoicesText("[grey](Move up and down to reveal more frameworks)[/]")
-                  .AddChoices(new[] {
-          "XUnit", "NUnit","MSTest"
-                  }));
-
-            AnsiConsole.Status()
-              .Start("Generating project...", ctx =>
-              {
-                  if (answerReadme)
-                  {
-                      AnsiConsole.MarkupLine("LOG: Creating README ...");
-                      Thread.Sleep(1000);
-                      // Update the status and spinner
-                      ctx.Status("Next task");
-                      ctx.Spinner(Spinner.Known.Star);
-                      ctx.SpinnerStyle(Style.Parse("green"));
-                  }
-
-                  if (answerGitIgnore)
-                  {
-                      AnsiConsole.MarkupLine("LOG: Creating .gitignore ...");
-                      Thread.Sleep(1000);
-                      // Update the status and spinner
-                      ctx.Status("Next task");
-                      ctx.Spinner(Spinner.Known.Star);
-                      ctx.SpinnerStyle(Style.Parse("green"));
-                  }
-
-                  // Simulate some work
-                  AnsiConsole.MarkupLine("LOG: Configuring test framework...")
-  ;
-                  Thread.Sleep(2000);
-              });
-
-        }
-
         private static async Task WriteTextFile(LinkedList<string> text, string path)
         {
             await File.WriteAllLinesAsync(path, text);
